ec4260b3fb4cc6c22a452408829abcdc597e1524
Faizan Ansari <faizansagheer346c@gamil.com>
2021-03-03 05:32:17 +0530

    public app.js code added and ui changes

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,25 +1,32 @@
+let selectedDate;
+let dateClickHandler = function(e){
+    selectedDate && selectedDate.classList.toggle("color-date")
+    this.classList.toggle("color-date");
+    selectedDate = this;
+}
 const setCalender = (year, month) => {
     let dateElements = document.querySelectorAll('td');
     let dateStart = new Date(year,month,1);
     let dateEnd = new Date(year,month+1,0);
     let date = 1;
+    dateStart = dateStart.getDay();
     for(let i = 0 ; i < 35; i++) {
-        if(i >= dateStart.getDay() && date <= dateEnd.getDate()) {
-            dateElements[i].addEventListener('click', function(e){
-                this.style.backgroundColor= "#f7d9d9";
-                console.dir(this)
-            })
+        if(i >= dateStart && date <= dateEnd.getDate()) {
+            dateElements[i].addEventListener('click', dateClickHandler)
             dateElements[i].innerText = date++;
         }
         else {
+            dateElements[i].removeEventListener('click', dateClickHandler)
             dateElements[i].innerText = "X"
         }
     }
+    dateElements[dateStart].click();
 }
 const todayDate = new Date();
 let year = todayDate.getFullYear()
 let month = todayDate.getMonth()
 setCalender(year, month);
+
 
 const monthSelect = document.querySelector('#month-option')
 const yearSelect = document.querySelector('#year-option')
