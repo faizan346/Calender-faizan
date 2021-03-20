b11abdee3e103d44514b9c5f7ea3b5fd4a720d1e
Faizan Ansari <faizansagheer346c@gamil.com>
2021-03-21 01:37:59 +0530

    minor error resolve in calender id

--- middleware.js
+++ middleware.js
@@ -1,6 +1,6 @@
 const { taskSchema } = require("./schemas")
 const Calender = require('./models/calender')
-const { ExpressError } = require('./utils/ExpressError')
+const ExpressError = require('./utils/ExpressError')
 
 module.exports.isLogin = function (req, res, next) {
     if (req.isAuthenticated()) {
@@ -9,21 +9,24 @@ module.exports.isLogin = function (req, res, next) {
     req.flash('error', 'You must be signed in first!');
     res.redirect("/login")
 }
+
 module.exports.isAuthor = async function (req, res, next) {
-    let calender = await Calender.findById(req.params.id)
-    if (calender) {
+    try {
+        let calender = await Calender.findById(req.params.id)
         if (calender.author.equals(req.user._id)) return next();
+        calender = await Calender.findOne({ author: req.user._id })
+        req.flash('error', "you don't have permission to do that")
+        res.redirect(`/calender/${calender._id}`)
+    } catch (e) {
+        next(new ExpressError(e.msg, 400))
     }
-    calender = await Calender.findOne({ author: req.user._id })
-    req.flash('error', "you don't have permission to do that")
-    res.redirect(`/calender/${calender._id}`)
+
 }
 
 module.exports.validateTask = function (req, res, next) {
     const { error } = taskSchema.validate(req.body);
-    console.log(req.body);
-    console.log(error)
     if (error) {
+        console.log(error)
         const msg = error.details.map(el => el.message).join(',')
         throw new ExpressError(msg, 400)
     } else {
